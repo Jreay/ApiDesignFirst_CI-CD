e69c712af65f0d57205e8b4f1856aa1ba6e2ca6b
Josue Rea <jrea314@outlook.com>
2025-06-11 06:55:31 -0500

    Ajustes V2

--- tests/test-k6.js
+++ tests/test-k6.js
@@ -11,7 +11,7 @@ export const options = {
     { duration: '30s', target: 0 },
   ],
   thresholds: {
-    http_req_failed: ['rate<0.01'],
+    http_req_failed: ['rate<0.10'],
     http_req_duration: ['p(95)<700'],
   },
 };
